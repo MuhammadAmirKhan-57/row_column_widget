0d000f42d3ade97af21451cb7700c69f1602d7dd
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-04 22:41:54 +0500

    Row Screen add

--- lib/Row_Column/row_screen.dart
+++ lib/Row_Column/row_screen.dart
@@ -18,6 +18,132 @@ class RowScreen extends StatelessWidget {
             ),
         backgroundColor: Colors.grey,
       ),
+      body: Padding(padding: EdgeInsets.all(25),
+      child: Column(
+        children: [
+          //Here is row screen , where it is horizental axis 
+          //Like first i have container then text then icon in one line
+          Row(
+            children: [
+               Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Colors.blueGrey,
+                  ),
+                  child: Center(
+                    child: Text(
+                      'R',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8,),
+                Text(
+                  'Ali Khan : Islamabad ,Pk',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(width: 10,),
+                  Icon(Icons.location_on, color: Colors.red, size: 20,)
+            ],
+          ),
+          SizedBox(height: 10),
+           Row(
+            children: [
+               Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Colors.blueGrey,
+                  ),
+                  child: Center(
+                    child: Text(
+                      'R',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8,),
+                Text(
+                  'Ali Khan : Islamabad ,Pk',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(width: 10,),
+                  Icon(Icons.location_on, color: Colors.red, size: 20,)
+            ],
+          ),
+          SizedBox(height: 10),
+           Row(
+            children: [
+               Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Colors.blueGrey,
+                  ),
+                  child: Center(
+                    child: Text(
+                      'R',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8,),
+                Text(
+                  'Ali Khan : Islamabad ,Pk',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(width: 10,),
+                  Icon(Icons.location_on, color: Colors.red, size: 20,)
+            ],
+          ),
+          SizedBox(height: 10),
+           Row(
+            children: [
+               Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Colors.blueGrey,
+                  ),
+                  child: Center(
+                    child: Text(
+                      'R',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8,),
+                Text(
+                  'Ali Khan : Islamabad ,Pk',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(width: 10,),
+                  Icon(Icons.location_on, color: Colors.red, size: 20,)
+            ],
+          ),
+        ],
+      ),),
     );
   }
 }
